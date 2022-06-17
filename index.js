04e2d5ec5419e151761fed88ba33a297c5b12286
Rowa <jrowah@gmail.com>
2022-06-18 00:43:16 +0300

    connects db

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const { request } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = 3000;
 
-const app = express();
+require("dotenv").config();
 
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
+
+const app = express();
+//a code snippet that allows us to accept the data in JSON format.
 app.use(express.json());
 
 app.listen(3000, () => {

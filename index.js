adcea3ef7e3ea736e03f76b2d3109925d2aaf9ba
Rowa <jrowah@gmail.com>
2022-06-17 23:36:18 +0300

    sets up server listening port

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+
+const PORT = 3000;
+
 const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log(`We are live on port ${PORT}`);
+});

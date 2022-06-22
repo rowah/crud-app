a1ddf71c22c10cbd3b2d72ab33af9b053449b64d
Rowa <jrowah@gmail.com>
2022-06-23 00:51:37 +0300

    posts data to db

--- model/model.js
+++ model/model.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
 
+const dataSchema = new mongoose.Schema({
+  name: {
+    required: true,
+    type: String,
+  },
+  age: {
+    required: true,
+    type: Number,
+  },
+});
+
+module.exports = mongoose.model("Data", dataSchema);
+
+// const mongoose = require("mongoose");
+// const dataSchema = new mongoose.Schema({
+//   name: {
+//     required: true,
+//     type: String,
+//   },
+//   age: {
+//     required: true,
+//     type: Number,
+//   },
+// });
+// module.exports = mongoose.model("Data", dataSchema);
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 module.exports = router;
 
+const Model = require("../model/model");
+
 //Post Method
 router.post("/post", (req, res) => {
-  res.send("Post API");
+  //res.send("Post API");
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+  });
+  try {
+    const dataToSave = data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 //Get all Method

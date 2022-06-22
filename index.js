657c40380f42b180757dc4265130ce1ac79f35c8
Rowa <jrowah@gmail.com>
2022-06-23 00:05:10 +0300

    db connection

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const mongoString = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("DB connected successfully!");
+});
 
 const app = express();
 
 app.use(express.json());
-
-require("dotenv").config();
-
-const mongoString = process.env.DATABASE_URL;
 
 app.listen(PORT, () => {
   console.log(`Server is live on ${PORT}. Check it out!`);

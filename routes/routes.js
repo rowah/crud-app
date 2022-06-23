eb354ed7716ddd7d8f1274d41f8cfd19b1850425
Rowa <jrowah@gmail.com>
2022-06-23 13:14:42 +0300

    deletes data based on ID

--- routes/routes.js
+++ routes/routes.js
@@ -57,6 +57,13 @@ router.patch("/update/:id", async (req, res) => {
 });
 
 //Delete by ID Method
-router.delete("/delete/:id", (req, res) => {
-  res.send("Delete by ID API");
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await Model.findByIdAndDelete(id);
+    res.send(`Document with ${data.name} has been deleted`);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+  //res.send("Delete by ID API");
 });
